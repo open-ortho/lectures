fac653303a2ad1f1596cf3dde80b67efce4cffe0
Toni Magni <afm@case.edu>
2024-10-10 00:56:58 -0400

    OIDISOP centralize javascript

---
+++ presentations/orthodontic-informatics/common.js
@@ -0,0 +1,40 @@
+// common.js
+
+// List of scripts to be dynamically loaded
+const scripts = [
+    '../../../dist/reveal.js',
+    '../../../plugin/notes/notes.js',
+    '../../../plugin/markdown/markdown.js',
+    '../../../plugin/highlight/highlight.js',
+];
+
+// Function to load a script and return a promise that resolves when the script loads
+function loadScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.onload = resolve;
+        script.onerror = reject;
+        document.head.appendChild(script);
+    });
+}
+
+// Load all scripts sequentially
+Promise.all(scripts.map(loadScript))
+    .then(() => {
+        // All scripts are loaded, now initialize Reveal.js
+        Reveal.initialize({
+            hash: true,
+            slideNumber: 'c/t',
+            transition: 'fade',
+            plugins: [RevealMarkdown, RevealHighlight, RevealNotes],
+        });
+
+        // Additional Reveal configuration
+        Reveal.configure({
+            pdfSeparateFragments: false,
+        });
+    })
+    .catch(error => {
+        console.error('Error loading scripts:', error);
+    });
